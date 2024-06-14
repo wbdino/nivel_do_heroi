ab7029ddab54af46060b5b6e44d46bb7ed1509bb
wbdino <dev.bdino@gmail.com>
2024-06-13 21:54:36 -0300

    colocando em pratica lógica de programação

---
+++ index.js
@@ -0,0 +1,29 @@
+let nomeDoHeroi = "Hurk";
+let xpDoHeroi = 900;
+
+if(xpDoHeroi < 1000){
+  xpDoHeroi = "Ferro"
+}
+else if(xpDoHeroi >= 1001 && xpDoHeroi < 2000){
+  xpDoHeroi = "Bronze";
+}
+else if(xpDoHeroi >= 2001 && xpDoHeroi < 5000){
+  xpDoHeroi = "Prata";
+}
+else if(xpDoHeroi >= 5001 && xpDoHeroi < 7000){
+  xpDoHeroi = "Ouro";
+}
+else if(xpDoHeroi >= 7001 && xpDoHeroi < 8000){
+  xpDoHeroi = "Platina";
+}
+else if(xpDoHeroi >= 8001 && xpDoHeroi < 9000){
+  xpDoHeroi = "Ascendente";
+}
+else if(xpDoHeroi >= 9001 && xpDoHeroi < 10000){
+  xpDoHeroi = "Imortal";
+}
+else if(xpDoHeroi >= 10001){
+  xpDoHeroi = "Radiante";
+}
+
+console.log("O Heroi de nome " + nomeDoHeroi + " esta no nivel de " + xpDoHeroi);
